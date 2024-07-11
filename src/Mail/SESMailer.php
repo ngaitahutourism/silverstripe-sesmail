f07107493c46109cb1dedfbc758fb982ae570df7
Merwin Poulose <merwinpoulose@gmail.com>
2024-07-11 12:02:38 +1200

    Update SESMailer.php
    Signed-off-by: Merwin Poulose <merwinpoulose@gmail.com>

--- src/Mail/SESMailer.php
+++ src/Mail/SESMailer.php
@@ -3,7 +3,7 @@
 namespace Symbiote\SilverStripeSESMailer\Mail;
 
 use Aws\Ses\SesClient;
-use SilverStripe\Control\Email\Mailer;
+use SilverStripe\Control\Email\Emailer;
 use SilverStripe\Control\Email\Email;
 use SilverStripe\Core\Injector\Injector;
 use Exception;
@@ -14,66 +14,66 @@ use PHPMailer\PHPMailer\PHPMailer;
 /**
  * A mailer implementation which uses Amazon's Simple Email Service.
  *
- * This mailer uses the SendRawEmail endpoint, so it supports sending attachments. Note that this only sends sending
- * emails to up to 50 recipients at a time, and Amazon's standard SES usage limits apply.
+ * This mailer uses the SendRawEmail endpoint, so it supports sending attachments. Note that this only sends emails
+ * to up to 50 recipients at a time, and Amazon's standard SES usage limits apply.
  *
  * Does not support inline images.
  */
-class SESMailer implements Mailer
+class SESMailer implements Emailer
 {
 
-	/**
-	 * @var SesClient
-	 */
-	protected $client;
+    /**
+     * @var SesClient
+     */
+    protected $client;
 
-	/**
+    /**
      * Uses QueuedJobs module when sending emails
      *
      * @var boolean
      */
-	protected $useQueuedJobs = true;
+    protected $useQueuedJobs = true;
 
-	/**
-	 * @var array|null
-	 */
-	protected $lastResponse = null;
+    /**
+     * @var array|null
+     */
+    protected $lastResponse = null;
 
-	/**
-	 * @param array $config
-	 */
-	public function __construct()
-	{
-		$config = Config::inst()->get('Symbiote\SilverStripeSESMailer\Mail\Config');
+    /**
+     * @param array $config
+     */
+    public function __construct()
+    {
+        $config = Config::inst()->get('Symbiote\SilverStripeSESMailer\Mail\Config');
         $this->client = SesClient::factory($config);
-	}
+    }
 
-	/**
-	 * @param boolean $bool
-	 *
-	 * @return $this
-	 */
-	public function setUseQueuedJobs($bool)
-	{
-		$this->useQueuedJobs = $bool;
+    /**
+     * @param boolean $bool
+     *
+     * @return $this
+     */
+    public function setUseQueuedJobs($bool)
+    {
+        $this->useQueuedJobs = $bool;
+        return $this;
+    }
 
-		return $this;
-	}
+    /**
+     * @return array|null
+     */
+    public function getLastResponse()
+    {
+        return $this->lastResponse;
+    }
 
-	/**
-	 * @return array|null
-	 */
-	public function getLastResponse()
-	{
-		return $this->lastResponse;
-	}
-
-	/**
-	 * @param SilverStripe\Control\Email
-	 */
-	public function send($email)
-	{
-        $config = Injector::inst()->get('SilverStripe\Control\Email\Mailer');
+    /**
+     * @param Email $email
+     * @return bool
+     */
+    public function send($email)
+    {
+        $config = Injector::inst()->get('Symbiote\SilverStripeSESMailer\Mail\Config');
         $from = array_keys($email->getFrom())[0];
         $to = array_keys($email->getTo())[0];
 
@@ -90,48 +90,36 @@ class SESMailer implements Mailer
         $mail->isHTML(true);
         $mail->Subject    = $email->getSubject();
         $mail->Body       = $email->body;
-        $mail->Send();
+        $mail->send();
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Send an email via SES. Expects an array of valid emails and a raw email body that is valid.
-	 *
-	 * @param array $destinations array of emails addresses this email will be sent to
-	 * @param string $rawMessageText Raw email message text must contain headers; and otherwise be a valid email body
-	 * @return Array Amazon SDK response
-	 */
-	public function sendSESClient ($destinations, $rawMessageText) {
+    /**
+     * Send an email via SES. Expects an array of valid emails and a raw email body that is valid.
+     *
+     * @param array $destinations array of emails addresses this email will be sent to
+     * @param string $rawMessageText Raw email message text must contain headers; and otherwise be a valid email body
+     * @return array Amazon SDK response
+     */
+    public function sendSESClient($destinations, $rawMessageText)
+    {
+        try {
+            $response = $this->client->sendRawEmail([
+                'Destinations' => $destinations,
+                'RawMessage' => ['Data' => $rawMessageText]
+            ]);
+        } catch (Exception $ex) {
+            if (strpos($ex->getMessage(), "cURL error 56") !== false) {
+                $response = $this->client->sendRawEmail([
+                    'Destinations' => $destinations,
+                    'RawMessage' => ['Data' => $rawMessageText]
+                ]);
+            } else {
+                throw $ex;
+            }
+        }
 
-		try {
-			$response = $this->client->sendRawEmail(array(
-				'Destinations' => $destinations,
-				'RawMessage' => array('Data' => $rawMessageText)
-			));
-		} catch (Exception $ex) {
-			/*
-			 * Amazon SES has intermittent issues with SSL connections being dropped before response is full received
-			 * and decoded we're catching it here and trying to send again, the exception doesn't have an error code or
-			 * similar to check on so we have to relie on magic strings in the error message. The error we're catching
-			 * here is normally:
-			 *
-			 * AWS HTTP error: cURL error 56: SSL read: error:00000000:lib(0):func(0):reason(0), errno 104
-			 * (see http://curl.haxx.se/libcurl/c/libcurl-errors.html) (server): 100 Continue
-			 *
-			 * Without the line break, so we check for the 'cURL error 56' as it seems likely to be consistent across
-			 * systems/sites
-			 */
-			if(strpos($ex->getMessage(), "cURL error 56")) {
-				$response = $this->client->sendRawEmail(array(
-					'Destinations' => $destinations,
-					'RawMessage' => array('Data' => $rawMessageText)
-				));
-			} else {
-				throw $ex;
-			}
-		}
-
-		return $response;
-	}
+        return $response;
+    }
 }
